206d6bd7a22e29a7d8766b0e4f348a9e3b37d1c0
diego64 <diegoferreira1964@gmail.com>
2024-06-13 20:03:46 -0300

    Creating User schema :sparkles:

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/auth_nest')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/auth_nest'), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { UserCreateDTO } from './user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './user.schema';
+import { Model } from 'mongoose';
+import { randomUUID } from 'crypto';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(userDTO: UserCreateDTO) {
+    const userCreated = new this.userModel({ ...userDTO, id: randomUUID() });
+    const user = userCreated.save();
+    console.log({ user });
+    return user;
+  }
+}
