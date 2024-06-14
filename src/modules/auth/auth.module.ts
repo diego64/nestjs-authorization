487bed0fb7a8b60d23d9411cac22e2ab612bdce7
diego64 <diegoferreira1964@gmail.com>
2024-06-13 21:49:14 -0300

    User authentication :passport_control:

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '../users/user.schema';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '15s' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/mongoose';
+import { compare } from 'bcrypt';
+import { User } from '../../modules/users/user.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    @InjectModel(User.name) private userModel: Model<User>,
+  ) {}
+
+  async login(username: string, password: string) {
+    const user = await this.userModel
+      .findOne({
+        username,
+      })
+      .exec();
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    const isEqualPassword = await compare(password, user.password);
+
+    if (!isEqualPassword) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = {
+      sub: user._id,
+      user: {
+        id: user.id,
+        username: user.username,
+        name: user.name,
+      },
+    };
+
+    const token = await this.jwtService.signAsync(payload);
+    return {
+      access_token: token,
+    };
+  }
+}
