bd5818e99b623f9f6f6a617e0b7f86de3cb5222d
diego64 <diegoferreira1964@gmail.com>
2024-06-14 21:07:27 -0300

    List of authorized products :sparkles:

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,16 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateProductUseCase } from './use-cases/create-product.usecase';
 import { CreateProductDTO } from './product.dto';
 import { Role, Roles } from 'src/decorators/roles.decorators';
+import { ListProductUseCase } from './use-cases/list.product.usecase';
 
 @Controller('products')
 export class ProductController {
-  constructor(private readonly createProductUseCase: CreateProductUseCase) {}
+  constructor(
+    private readonly createProductUseCase: CreateProductUseCase,
+    private listProductUseCase: ListProductUseCase,
+  ) {}
 
   @Roles(Role.ADMIN)
   @Post('')
   async create(@Body() data: CreateProductDTO) {
     const result = await this.createProductUseCase.execute(data);
+    return result;
+  }
+
+  @Roles(Role.USER, Role.ADMIN)
+  @Get('')
+  async get() {
+    const result = await this.listProductUseCase.execute();
     return result;
   }
 }
